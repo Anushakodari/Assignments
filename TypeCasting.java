3b616911f97899155aa2192866c879d9fd8ee473
Anushakodari <98135834+Anushakodari@users.noreply.github.com>
2022-02-02 10:50:11 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+package com.simplilearn.program;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		String s = "100";
+		int i = Integer.parseInt(s);
+		System.out.println(i);
+		
+
+	}
+
+}
