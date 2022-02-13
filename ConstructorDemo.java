70e67de1f8a2e9bc57e8f2ba297ad9cf8b1e2ba1
Anushakodari <98135834+Anushakodari@users.noreply.github.com>
2022-02-13 13:16:00 +0530

    Add files via upload

---
+++ ConstructorDemo.java
@@ -0,0 +1,52 @@
+package com.simplilearn.program;
+
+
+
+	class EmpInfo{
+		int id;
+		String name;
+
+	void display() {
+		System.out.println(id+" "+name);
+		}
+	}
+
+	public class ConstructorDemo {
+
+	public static void main (String[] args) {
+
+		EmpInfo emp1=new EmpInfo();
+		EmpInfo emp2=new EmpInfo();
+
+		emp1.display();
+		emp2.display();
+		}
+	
+
+	//parameterized constructor
+	class Std{
+		int id;
+		String name;
+
+		Std(int i,String n)
+		{
+		id=i;
+		name=n;
+		}
+
+		void display(int i) {
+		System.out.println(id+" "+name);
+		}
+	}
+
+	public class paramConstrDemo {
+		public void main (String[] args) {
+		Std std1=new Std(2,"Alex");
+		Std std2=new Std(10,"Annie");
+		std1.display(2);
+		std2.display(10);
+			}
+	}
+	}
+
+
