970613b2e53ec3d2b5d7faf3e7a471ee4de60ac6
Anushakodari <98135834+Anushakodari@users.noreply.github.com>
2022-02-20 16:39:36 +0530

    Add files via upload

---
+++ MapDemo.java
@@ -0,0 +1,16 @@
+package com.simplilearn.program;
+import java.util.*;
+
+import java.util.Map.Entry;
+public class MapDemo { 
+	 public static void main(String args[]){  
+	  Map<Integer,String> map=new HashMap<Integer,String>();  
+	  map.put(100,"Amit");  
+	  map.put(101,"Vijay");  
+	  map.put(102,"Rahul");  
+	  //Elements can traverse in any order  
+	  for(Map.Entry m:map.entrySet()){  
+	   System.out.println(m.getKey()+" "+m.getValue());  
+	  }  
+	 }  
+	}  
